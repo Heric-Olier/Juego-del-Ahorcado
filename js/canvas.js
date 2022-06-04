0a6aa60dcafe3364ff43be592a511c256a702dc0
Heric Olier <hericolier@gmail.com>
2022-06-04 17:58:50 -0500

    update notifications & UX-UI

--- js/canvas.js
+++ js/canvas.js
@@ -185,7 +185,7 @@ const notification = (text,  img) => {
 
 // creamos la funcion de volver al inicio cuando el usuario presione el boton de rendirse
 const leaveGame = () => {
-  setTimeout(() => {window.open("../index.html", "_self")}, 1800);
+  setTimeout(() => {window.open("../Juego-del-Ahorcado/index.html", "_self")}, 1800);
   notification(`La palabra secreta era ${sortedWord} `, 'assets/icon-head-lose.svg');
 }
 btnLeaveGame.addEventListener('click', leaveGame);
